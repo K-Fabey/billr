398caacfa85b31e63850bc1f15cfee13efcbf132
gary camboulives <gary.camboulives@gmail.com>
2022-03-02 16:14:49 +0100

    good seeds no front

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -9,7 +9,7 @@ class InvoicesController < ApplicationController
     @invoice = Invoice.new
     @invoice.sender = company.find(params[:sender_id])
     # @invoice.recipient = company.find(params[:recipient_id])
-
+    authorize @invoice
     if @invoice.save!
       redirect_to_invoice_path(@invoice)
     else
@@ -30,9 +30,8 @@ class InvoicesController < ApplicationController
 
   def received
     @user = current_user
-    #@invoices = policy_scope(Invoice)
     current_company = current_user.company
-    @received_invoices = Invoice.where( recipient: current_company)
+    @received_invoices = current_company.received_invoices
     @invoices = @received_invoices
     authorize @invoices
     render :index
@@ -40,9 +39,8 @@ class InvoicesController < ApplicationController
 
   def sent
     @user = current_user
-    # @invoices = policy_scope(Invoice)
     current_company = current_user.company
-    @sent_invoices = Invoice.where( sender: current_company)
+    @sent_invoices = current_company.sent_invoices
     @invoices = @sent_invoices
     authorize @invoices
     render :index
@@ -85,5 +83,4 @@ class InvoicesController < ApplicationController
   def set_invoice
     @invoice = Invoice.find(params[:id])
   end
-
 end
