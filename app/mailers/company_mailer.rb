f1b875b59ce013491499ee9b159a5a9833093052
Fabrice KANA <fabricekana10@gmail.com>
2022-03-08 14:40:25 +0100

    fixing conflicts

--- app/mailers/company_mailer.rb
+++ app/mailers/company_mailer.rb
@@ -33,9 +33,7 @@ class CompanyMailer < ApplicationMailer
     end
 
     # mail to: @company.email, subject: "Nouvelle facture"
-<<<<<<< HEAD
-    mail to: @company.email, subject: "#{@user.company.name} - Nouvelle facture"
-=======
+
     mail(
       subject: "#{@user.company.name}- Nouvelle facture",
       to: @company.email,
@@ -43,7 +41,6 @@ class CompanyMailer < ApplicationMailer
       html_body: '',
       track_opens: 'true',
       message_stream: 'outbound')
->>>>>>> ce77c3437fdf7ec9d6446e1209ea1aca74abc7ea
   end
 
   def send_followup(invoice, user)
@@ -56,9 +53,8 @@ class CompanyMailer < ApplicationMailer
 
     # attachments['invoice_test.pdf'] = File.read('app/assets/images/INV-004863-Orange.pdf')
     # mail to: @company.email, subject: "Nouvelle facture"
-<<<<<<< HEAD
-    mail to: @company.email, subject: "#{@user.company.name} - Facture impayée"
-=======
+
+
     # mail to: @company.email, subject: "#{@user.company.name} - Facture impayée"
 
     if @invoice.invoice_file.attached?
@@ -67,7 +63,6 @@ class CompanyMailer < ApplicationMailer
       attachments['invoice.pdf'] = File.read('app/assets/images/INV-004863-Orange.pdf')
     end
 
-
       mail(
     subject: " #{@user.company.name}- Facture impayée",
     to: @company.email,
@@ -75,7 +70,7 @@ class CompanyMailer < ApplicationMailer
     html_body: '',
     track_opens: 'true',
     message_stream: 'outbound')
->>>>>>> ce77c3437fdf7ec9d6446e1209ea1aca74abc7ea
+
   end
 
 end
