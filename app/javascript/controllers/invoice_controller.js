1a45c1f7d38009f6f3649ec05327bba76db0f75a
gary camboulives <gary.camboulives@gmail.com>
2022-03-09 16:56:37 +0100

    goodlast

--- app/javascript/controllers/invoice_controller.js
+++ app/javascript/controllers/invoice_controller.js
@@ -4,12 +4,12 @@ export default class extends Controller {
   static targets = ['form']
 
   connect() {
-    // console.log('Hello, Stimulus!');
+     console.log('Hello, Stimulus!');
     // console.log(this.formTarget);
 
   }
   decline(event) {
-    // console.log('super decline')
+    console.log('click bouton refuser')
     event.preventDefault();
     this.formTarget.classList.toggle('d-none');
   }
