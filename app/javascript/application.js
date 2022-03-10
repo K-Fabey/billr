6175170221e76d2d1bbde31246a135536735bb62
gary camboulives <gary.camboulives@gmail.com>
2022-03-10 17:51:27 +0100

    nepasmerge

---
+++ app/javascript/application.js
@@ -0,0 +1,24 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+import "controllers"
+import "@hotwired/turbo-rails"
+import "bootstrap"
+import "chartkick"
+import "chartkick/chart.js"
+import { initFlatpickr } from "./plugins/flatpickr";
+
+document.addEventListener('turbolinks:load', () => {
+  initFlatpickr();
+});
