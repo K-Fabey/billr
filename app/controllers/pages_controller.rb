98e1eea3ee232ff5f1b07d5444f8376203764bf4
Martin Dubois <martindubois1602@gmail.com>
2022-03-10 17:36:00 +0100

    Bon

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,18 +9,18 @@ class PagesController < ApplicationController
 
     data1 = current_user.company.received_invoices.group(:status).sum(:total_w_tax).transform_values{ |value| value.fdiv(1000).round(1)}
      @pie_data1 =  {
-        "Paiement en cours" => data1["payment in process"],
-        "Payées" => data1["paid"],
-        "Reçues" => data1["received"],
+        "En attente" => data1["received"],
+        "Validées" => data1["validated"],
         "Refusées" => data1["declined"],
-        "Validées" => data1["validated"]
+        "En attente de paiement" => data1["payment in process"],
+        "Payées" => data1["paid"]
       }
       data2 = current_user.company.sent_invoices.group(:status).sum(:total_w_tax).transform_values{ |value| value.fdiv(1000).round(1)}
       @pie_data2 =  {
-        "Payées" => data2["paid"],
         "Créées" => data2["created"],
         "Envoyées" => data2["sent"],
-        "Relancées" => data2["follow_uped"]
+        "Payées" => data2["paid"],
+        "En retard de paiement" => data2["follow_uped"]
       }
   end
 end
